fdb849bf6946568c2b8f1ed8e78856de512f3e3f
Justin Bui <justinqbui@gmail.com>
2024-01-30 22:18:07 -0800

    change to camelcase

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -3,32 +3,32 @@ import {modelOptions, prop, PropType} from "@typegoose/typegoose";
 @modelOptions({ schemaOptions: { collection: 'room'}})
 export class RoomSchema {
   @prop({type: () => String, required: true})
-  public host_ws_id: string;
+  public HostWsId: string;
 
   @prop({required: true, type: () => Array<String>})
-  public player_ids: Array<string>;
+  public PlayerIds: Array<string>;
 
   @prop({type: () => String, required: true})
-  public room_state: string;
+  public RoomState: string;
 }
 
 @modelOptions({ schemaOptions: { collection: 'quiz'}})
 export class QuizSchema {
     @prop({required: true, type: () => Array<String>}, PropType.ARRAY)
-    public questions: Array<string>;
+    public Questions: Array<string>;
 }
 
 @modelOptions({ schemaOptions: { collection: 'question'}})
 export class QuestionSchema {
     @prop({type: () => String, required: true})
-    public question: string;
+    public Question: string;
 
     @prop({type: () => String, required: true}, PropType.MAP)
-    public possible_answers: Map<string, string>;
+    public PossibleAnswers: Map<string, string>;
 
     @prop({type: () => String, required: true})
-    public correct_answer: string;
+    public CorrectAnswer: string;
 
     @prop({type: () => String, default: 'MCQ'})
-    public question_type?: string;
+    public QuestionType?: string;
 }
